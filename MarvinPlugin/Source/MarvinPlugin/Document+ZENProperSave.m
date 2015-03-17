9bc5161ace4226136c5a4d5239c4b30ead0bba1c
Christoffer Winterkvist <chris@zenangst.com>
2015-03-17 09:26:28 +0100

    Swizzle proper save instead of having an additional menu items

--- MarvinPlugin/Source/MarvinPlugin/Document+ZENProperSave.m
+++ MarvinPlugin/Source/MarvinPlugin/Document+ZENProperSave.m
@@ -19,9 +19,11 @@
         [[NSNotificationCenter defaultCenter] postNotificationName:@"Save properly" object:nil];
     }
 
-    [self zen_saveDocumentWithDelegate:delegate
-                        didSaveSelector:didSaveSelector
-                            contextInfo:contextInfo];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self zen_saveDocumentWithDelegate:delegate
+                           didSaveSelector:didSaveSelector
+                               contextInfo:contextInfo];
+    });
 }
 
 + (void)load {
