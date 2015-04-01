ed31d0e00a53a1c5c41d42fbba96bf03296c06cd
Christoffer Winterkvist <chris@zenangst.com>
2015-04-01 09:42:23 +0200

    Add zen_handleTextViewShouldChangeTextInRange

--- MarvinPlugin/Source/MarvinPlugin/DVTTextCompletionSession+ChangeMarks.m
+++ MarvinPlugin/Source/MarvinPlugin/DVTTextCompletionSession+ChangeMarks.m
@@ -12,6 +12,19 @@
 
 @implementation DVTTextCompletionSession (ChangeMarks)
 
+- (BOOL)zen_handleTextViewShouldChangeTextInRange:(struct _NSRange)arg1 replacementString:(id)arg2
+{
+    long long selectedCompletionIndex = [self selectedCompletionIndex];
+    NSArray *allCompletions = [self filteredCompletionsAlpha];
+    IDEIndexCompletionItem *completion = allCompletions[selectedCompletionIndex];
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"Add change mark" object:[completion completionText]];
+    });
+
+    return [self zen_handleTextViewShouldChangeTextInRange:arg1 replacementString:arg2];
+}
+
 - (BOOL)zen_insertCurrentCompletion
 {
     long long selectedCompletionIndex = [self selectedCompletionIndex];
@@ -25,9 +38,22 @@
     return [self zen_insertCurrentCompletion];
 }
 
+- (BOOL)zen_handleInsertText:(id)arg1
+{
+    return [self zen_handleInsertText:arg1];
+}
+
 + (void)load
 {
     Method original, swizzle;
+
+    original = class_getInstanceMethod(self, NSSelectorFromString(@"handleTextViewShouldChangeTextInRange:replacementString:"));
+    swizzle = class_getInstanceMethod(self, NSSelectorFromString(@"zen_handleTextViewShouldChangeTextInRange:replacementString:"));
+    method_exchangeImplementations(original, swizzle);
+
+    original = class_getInstanceMethod(self, NSSelectorFromString(@"handleInsertText:"));
+    swizzle = class_getInstanceMethod(self, NSSelectorFromString(@"zen_handleInsertText:"));
+    method_exchangeImplementations(original, swizzle);
 
     original = class_getInstanceMethod(self, NSSelectorFromString(@"insertCurrentCompletion"));
     swizzle = class_getInstanceMethod(self, NSSelectorFromString(@"zen_insertCurrentCompletion"));
