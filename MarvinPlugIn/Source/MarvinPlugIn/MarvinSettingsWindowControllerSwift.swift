55f0634da25ec3a6dfb04b252e4021059e924dd0
Christoffer Winterkvist <christoffer@hyper.no>
2015-12-20 20:11:42 +0100

    Refactor code to not show window on launch

--- MarvinPlugIn/Source/MarvinPlugIn/MarvinSettingsWindowControllerSwift.swift
+++ MarvinPlugIn/Source/MarvinPlugIn/MarvinSettingsWindowControllerSwift.swift
@@ -2,11 +2,17 @@ import Foundation
 
 class MarvinSettingsWindowController: NSWindowController {
 
+  var bundle: NSBundle?
   @IBOutlet weak var shouldRemoveWhitespace: NSButton?
 
   convenience init(bundle: NSBundle) {
     self.init(window: nil)
+    self.bundle = bundle
+  }
 
+  func openWindow() {
+    guard let bundle = bundle else { return }
     bundle.loadNibNamed("Settings", owner: self, topLevelObjects: nil)
+    showWindow(self)
   }
 }
