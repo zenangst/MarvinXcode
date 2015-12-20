6c47526eafbc8a86a5ef8e6816437c0578b12b5d
Christoffer Winterkvist <christoffer@hyper.no>
2015-12-20 19:13:44 +0100

    Remove constants

--- MarvinPlugin/Source/MarvinPlugin/XcodeManager.h
+++ MarvinPlugin/Source/MarvinPlugin/XcodeManager.h
@@ -1,9 +1,5 @@
 #import <Foundation/Foundation.h>
 
-static NSString *const kMarvinValidSetWordString = @"0123456789ABCDEFGHIJKOLMNOPQRSTUVWXYZÅÄÆÖØabcdefghijkolmnopqrstuvwxyzåäæöø_";
-static NSString *const kMarvinSpaceSet = @"#-<>/(){}[],;:. \n`*\"'	";
-static NSString *const kMarvinValidLineRange = @"0123456789ABCDEFGHIJKOLMNOPQRSTUVWXYZÅÄÆÖØabcdefghijkolmnopqrstuvwxyzåäæöø_!\"#€%&/()=?`<>@£$∞§|[]≈±´¡”¥¢‰¶\{}≠¿`~^*+-;.";
-
 @interface XcodeManager : NSObject
 
 @property (nonatomic) NSRange selectedRange;
